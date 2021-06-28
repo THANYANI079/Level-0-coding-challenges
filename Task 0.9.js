3e3a1262ec21da965d0b49309e14d65add3dcb0a
THANYANI079 <80773520+THANYANI079@users.noreply.github.com>
2021-06-28 19:43:26 +0200

    Update Task 0.9.js

--- Task 0.9.js
+++ Task 0.9.js
@@ -1,12 +1,12 @@
-
-
-var printVowels = str => {
-  var vowels=['a','e','i','o','u'];
-  var str_array=str.replace(/[^a-zA-Z]/g, '').split('');
-
-  var vowels_filtered=str_array.filter( a => vowels.includes(a.toLowerCase()));
-
-  return vowels_filtered.join(', ');
+function vowels(str){
+    var input = [];
+    var tmpVowels = str.replace(/[^aeiou]/gi,"").split("");
+    for(var i = 0; i < tmpVowels.length; i++){
+        if(input.indexOf(tmpVowels[i].toLowerCase()) == -1){
+           input.push(tmpVowels[i].toLowerCase());
+        }          
+    }
+    return input.toString();
 }
 
-console.log(printVowels('IshmAel'))
+console.log(vowels("muswazu"));
