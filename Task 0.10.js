68fc6d1f297f6a22de0091f732c428295e444fbb
THANYANI079 <80773520+THANYANI079@users.noreply.github.com>
2021-06-30 17:17:15 +0200

    Update Task 0.10.js

--- Task 0.10.js
+++ Task 0.10.js
@@ -1,22 +1,38 @@
+    
+    var maxInput = 26;
 
-function common() {
-  var list = {};
+    function commonLetters(str1,str2)
+    {
+
+        let result1 = new Array(maxInput); 
+        let result2 = new Array(maxInput);
+      
+        for(let i=0;i<maxInput;i++)
+        {
+            result1[i]=0;
+            result2[i]=0;
+        }
+   
+        let length1 = str1.length;
+        let length2 = str2.length;
+   
+        for (let i = 0 ; i < length1 ; i++)
+           result1[str1[i].charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+   
+        for (let i = 0 ; i < length2 ; i++)
+           result2[str2[i].charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+   
   
-  for(var i=0; i < str_1.length; i++) {
-    list[str_1.charAt(i)] = 1;
-  }
- 
-  const commonLetters =[]; 
-  for(var i=0; i < str_2.length; i++) {
-    if( list[str_2.charAt(i)] == 1) {
-      commonLetters.push(str_2.charAt(i));
+        for (let i = 0 ; i < maxInput ; i++)
+        {
+            if (result1[i] != 0 && result2[i] != 0)
+            {
+              
+                for (let j = 0 ; j < Math.min(result1[i], result2[i]) ; j++)
+                    console.log((String.fromCharCode(i + 'a'.charCodeAt(0))));
+            }
+        }
     }
-  }
-  
-  list = commonLetters.join(" ");
-  console.log(list);
-}
-const  str_1= "string";
-const  str_2 ="strong";
-
-common();
+     
+    let str1 ="housse", str2 = "computers";
+    commonLetters(str1, str2);   
